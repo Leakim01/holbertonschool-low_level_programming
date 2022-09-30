71aa2178a712504450b4080279be363cf4be7993
Leakim01 <mickael.mangin@live.fr>
2022-09-30 10:23:57 -0700

    Task 5 - Write with printf a C Program that Prints a Text with a New Line

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   * puts - Diplay the text
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * printf - Diplay the text
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,");
