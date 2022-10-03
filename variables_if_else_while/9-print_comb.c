d92de16ba9079273720f576a0baf0a0d5d66d858
Leakim01 <mickael.mangin@live.fr>
2022-10-03 12:49:36 -0700

    Task 9 - How to Print All numbers Base10 Separated by Virgule and Space

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* more headers goes there
+*
+* betty style doc for function main goes there
+*
+* Return: always 0 value
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+		putchar(',');
+		putchar(' ');
+			putchar('\n');
+
+	return (0);
+}
