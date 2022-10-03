7bfcbb43b4bd4c42f286f5c63bf6332a754b6903
Leakim01 <mickael.mangin@live.fr>
2022-10-03 08:20:53 -0700

    Task 1 - Create a Variable Printing

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,15 +23,15 @@ int main(void)
 
 	if (m > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", m = n%10);
+		printf("Last digit of %d is %d and is greater than 5\n", n);
 	}
 	else if (m < 6 && m != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", m = n%10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
 	}
 	else
 	{
-		printf("Last digit of %d is %d is zero\n", m = n%10);
+		printf("Last digit of %d is %d is zero\n", n);
 	}
 	return (0);
 }
