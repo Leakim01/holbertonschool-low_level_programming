6ddaaf1944da07743ef7fb1c224b64f36c682143
Leakim01 <mickael.mangin@live.fr>
2022-10-03 12:03:41 -0700

    Task 4 - How to Print Alphabet Without q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+* *
+* more headers goes there
+*
+* betty style doc for function main goes there
+*
+* Return: always 0 value
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	if (c != 'q' && c != 'e')
+		putchar(c);
+
+			putchar('\n');
+
+		return (0);
+}
