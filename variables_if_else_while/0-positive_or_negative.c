38e966906e9d9db40faf6a28cf00a60bd892ef4e
Leakim01 <mickael.mangin@live.fr>
2022-10-03 06:01:12 -0700

    Task 0 - Create a Variable Printing a Number with if and else

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /* more headers goes there */
 /* main find random number */
 /* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
