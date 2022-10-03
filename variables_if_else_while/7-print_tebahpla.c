b56f509923cad1b409cdfb58d2fde79387fa2463
Leakim01 <mickael.mangin@live.fr>
2022-10-03 12:26:24 -0700

    Task 7 - How to Print Alphabet Reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* more headers goes there
+*
+* betty style doc for function main goes there
+*
+* Return: always 0 value
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
