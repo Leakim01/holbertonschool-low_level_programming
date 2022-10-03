e204881b392e165287f4c7bd8da2ce4340e366d3
Leakim01 <mickael.mangin@live.fr>
2022-10-03 10:51:09 -0700

    Task 3 - How to Print Alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry Point
+ *
+ * more headers goes there
+ *
+ * betty style doc for function main goes there
+ *       
+ * Return: always 0 value
+ */
+
+int main(void)
+{
+	char c;
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	return (0);
+}
