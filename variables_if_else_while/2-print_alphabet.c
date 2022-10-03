c079041988354e6add96a9a276529b93a7b62431
Leakim01 <mickael.mangin@live.fr>
2022-10-03 11:14:10 -0700

    Task 3 - How to Print Alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,14 @@
  * more headers goes there
  *
  * betty style doc for function main goes there
- *       
+ *
  * Return: always 0 value
  */
 
 int main(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
 	putchar('\n');
