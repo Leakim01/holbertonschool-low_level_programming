8e71568494a4d4e63edfb3df2553a84851dedc5c
Leakim01 <mickael.mangin@live.fr>
2022-10-03 11:31:26 -0700

    Task 3 - How to Print Alphabet in Upper et Lowercase

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+/**
+* main - Entry Point
+*
+* more headers goes there
+*
+* betty style doc for function main goes there
+*
+* Return: always 0 value
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+
+		putchar('\n');
+
+	char C;
+
+	for (C = 'A'; C <= 'Z'; C++)
+	putchar(C);
+		putchar('\n');
+
+	return (0);
+}
