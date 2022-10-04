d99ad05d67a3014a45f6ccb7ea45f94da6914a53
Leakim01 <mickael.mangin@live.fr>
2022-10-04 06:50:13 -0700

    Task 0 - How to Print -putchar Without printf

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* more headers goes there
+*
+* betty style doc for function main goes there
+*
+* Return: always 0 value
+*/
+
+int main(void)
+{
+	char _putchar;
+
+	putchar(_putchar);
+
+		putchar('\n');
+
+	return (0);
+}
