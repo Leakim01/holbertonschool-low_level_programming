e28181b3b5e824f4bbd3f95f8c061fcf75a4f615
Leakim01 <mickael.mangin@live.fr>
2022-10-06 08:07:49 -0700

    Task 2 - How to Print x10 Alphabet in Lowercase with _putchar

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -17,11 +17,14 @@ void print_alphabet_x10(void)
 	int i, j;
 
 	for (i = 'a'; i <= 'z'; i++)
-	for (j = '0'; j <= '10'; j++)
-
 	{
-		_putchar(i);
-		_putchar(j);
+		for (j = '0'; j <= 'A'; j++)
+
+		{
+	
+			_putchar(i);
+		}
+
 		_putchar('\n');
 
 	}
