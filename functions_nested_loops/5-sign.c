e253eeb66cde0a92688ecbfbab35c211df5d0b4b
Leakim01 <mickael.mangin@live.fr>
2022-10-06 13:08:24 -0700

    Task 5 - Print the Sign of a Number with if

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,33 +2,28 @@
 
 /**
 * print_sign - Entry Point
-* @n: Character
+* @n: input
 *
 * more headers goes there
 *
 * betty style doc for function main goes there
 *
-* Return: always 0 value
+* Return: 1, 0 or -1
 */
 
 int print_sign(int n)
 {
-	if (n > '0')
-
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-
-	else if (n == '0')
-
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-
 	else
-
 	{
 		_putchar('-');
 		return (-1);
