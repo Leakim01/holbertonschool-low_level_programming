75f24c1233b055d71c13ae71bab4e8217a5ab4aa
Leakim01 <mickael.mangin@live.fr>
2022-10-06 06:12:04 -0700

    Task 1 - How to Print Alphabet in Lowercase with _putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry Point
+*
+* more headers goes there
+*
+* betty style doc for function main goes there
+*
+* Return: always 0 value
+*/
+
+void print_alphabet(void);
+
+{
+	int i = a
+	for (i <= z, c++);
+		
+		putchar(i);
+
+		putchar('\n')
+
+	
+	return (0);
+}
