477431f3f7c66e0a4e907d6af27249d4fa3f65e7
Leakim01 <mickael.mangin@live.fr>
2022-10-06 11:10:48 -0700

    Task 3 - Return 1 or 0 if c=lowercase

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,9 +1,13 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 #include <unistd.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 
 #endif
