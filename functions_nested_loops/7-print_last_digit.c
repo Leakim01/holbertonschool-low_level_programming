cbc58ea9939db2e577ec20f59b1ddd2153a02119
Leakim01 <mickael.mangin@live.fr>
2022-10-07 02:06:38 -0700

    Task 7 - Print the Lastdigit of i

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_last - Entry Point
+* @i: input
+*
+* more headers goes there
+*
+* betty style doc for function main goes there
+*
+* Return: 1, 0 or -1
+*/
+
+int print_last_digit(int i);
+
+{
+	_putchar(i % 10);
+	return (i %c 10);
+
+}
