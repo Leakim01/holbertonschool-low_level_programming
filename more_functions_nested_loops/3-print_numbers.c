0d824ec9717ad9d5885fd01b6c9bf455234130e7
Leakim01 <mickael.mangin@live.fr>
2022-10-08 09:36:09 -0700

    Task 3 - Display from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - Entry Point
+ *
+ * more headers goes there
+ *
+ * betty style doc for function main goes there
+ *
+ * Return: always 0 value
+ */
+void print_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+
+	{
+		_putchar(i);
+
+	}
+
+	_putchar('\n');
+
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _putchar(char c);
+void print_numbers(void);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
