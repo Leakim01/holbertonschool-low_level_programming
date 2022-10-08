d938615f73f0d89ed6524a04a526c00d34be6ace
Leakim01 <mickael.mangin@live.fr>
2022-10-08 08:27:37 -0700

    Task 2 - Multiply two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * mul - Entry Point
+ * @a, b: input
+ *
+ * more headers goes there
+ *
+ * betty style doc for function main goes there
+ *
+ * Return: The Last Digit of a Number
+ */
+
+int mul(int a, int b)
+{
+
+	for (a = '0'; a <= '9'; a++)
+	{	for (b = '0'; b <= '9'; a++)
+
+	return (0);
+	}
+}
