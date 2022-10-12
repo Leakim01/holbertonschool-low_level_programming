162a258e013bc80d37751eeb33fbae0a1520300b
Leakim01 <mickael.mangin@live.fr>
2022-10-12 09:43:48 -0700

    Task 6 - Print Every Character Starting 1st Char

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - Entry Point
+ * @s: Strings
+ *
+ * More Headers Goes There
+ *
+ * Betty style doc for function main goes there
+ *
+ * Return: always 0 value
+ */
+
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		_putchar (s[i]);
+	}
+	_putchar ('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <string.h>
 
+void puts2(char *str);
+void rev_string(char *s);
 void print_rev(char *s);
 void _puts(char *str);
 int _strlen(char *s);
