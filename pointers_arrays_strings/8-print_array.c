8d34d08993019de93816197b4561fe211fbf3944
Leakim01 <mickael.mangin@live.fr>
2022-10-13 02:48:31 -0700

    Task 8 - Print n Elements of an Array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - Entry Point
+ * @a: Array
+ * @n: Number of elements
+ *
+ * More Headers Goes There
+ *
+ * Betty style doc for function main goes there
+ *
+ * Return: always 0 value
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+		printf (*a[i]);
+	}
+	{	if (i != 0)
+		printf(", %d", *a[i]);
+	}
+	printf ('\n');
+}
