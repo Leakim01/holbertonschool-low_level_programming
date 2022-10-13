8fbb7b1e94afc84a844c536906d12411180477ce
Leakim01 <mickael.mangin@live.fr>
2022-10-13 04:53:02 -0700

    Task 8 - Print n Elements of an Array

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - Entry Point
+ * @str: Strings
+ *
+ * More Headers Goes There
+ *
+ * Betty style doc for function main goes there
+ *
+ * Return: always 0 value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
