161687b559a67bb70292630045250290a4d0dcce
Leakim01 <mickael.mangin@live.fr>
2022-10-16 10:07:12 -0700

    Task 6 - Change Each 1rt Letter of Sentences in Upper

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string - Entry Point
+ * @ptr: String
+ *
+ * More Headers Goes There
+ *
+ * Betty style doc for function main goes there
+ *
+ * Return: always 0 value
+ */
+
+char *cap_string(char *ptr)
+{
+	char sep[] = ", ;.!?\"(){}";
+	int i;
+	int n;
+
+	for (i = 0; ptr[i] != '\0'; i++)
+	{
+		for (n = 0; sep[n] != '\0'; n++)
+		{
+			if (ptr[i] == sep[n])
+				ptr[i] = ptr[i] - 32;
+		}
+	}
+	return (ptr);
+}
