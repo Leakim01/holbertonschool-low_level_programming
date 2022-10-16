38236e3e19790e4ba55d068c7287dd840c08e797
Leakim01 <mickael.mangin@live.fr>
2022-10-16 08:44:28 -0700

    Task 4 - Prints Revers a String

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - Entry Point
+ * @a: Array
+ * @n: Number of Arrays
+ *
+ * More Headers Goes There
+ *
+ * Betty style doc for function main goes there
+ *
+ * Return: always 0 value
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
+	}
+}
+		
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 
+void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
