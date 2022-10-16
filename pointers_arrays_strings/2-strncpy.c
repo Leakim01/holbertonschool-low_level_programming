5dccf5e16e8e9e379c6ccedaf5164be90935ebcd
Leakim01 <mickael.mangin@live.fr>
2022-10-16 03:52:55 -0700

    Task 1 - Print String Until n Byte

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - Entry Point
+ * @dest: Destination
+ * @src: Source
+ *
+ * More Headers Goes There
+ *
+ * Betty style doc for function main goes there
+ *
+ * Return: always 0 value
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	return (dest);
+}
