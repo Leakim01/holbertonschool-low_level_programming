7568fc477c147320f5b43d3c118e3987a0d366c2
Leakim01 <mickael.mangin@live.fr>
2022-10-18 03:13:30 -0700

    Task 0 - Print the Name of the Program

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * @argc: Place of each Argument
+ * @argv: Number of Argument (Size of Array)
+ *
+ * More Headers Goes There
+ *
+ * Betty style doc for function main goes there
+ *
+ * Return: always 0 value
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (; i <= argc; i++)
+	{
+		printf("%s", argv[0]);
+	}
+	return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,33 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+char *leet(char *);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+int _putchar(char c);
+void print_numbers(void);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
