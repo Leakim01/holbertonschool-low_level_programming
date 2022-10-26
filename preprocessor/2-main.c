7ed15be8bd5555bc33788f330654d2d4807e9243
Leakim01 <mickael.mangin@live.fr>
2022-10-26 06:55:50 -0700

    Task 2 - Program Prints the file it was Cmopiled from

---
+++ preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * More Headers Goes There
+ *
+ * Betty style doc for function main goes there
+ *
+ * Return: always 0 value
+ */
+
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
