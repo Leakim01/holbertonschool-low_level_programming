59d948012d0396a89130531164ff33c049f482e0
Leakim01 <mickael.mangin@live.fr>
2022-10-31 06:17:58 -0700

    Task 1 - Initialize a Variable

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+/**
+ * #include "dog.h"
+ * #include "main.h"
+ *
+ * init_dog - Short description
+ * @d: pointer
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+void print_dog(struct dog *d)
+{
+}
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <math.h>
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
