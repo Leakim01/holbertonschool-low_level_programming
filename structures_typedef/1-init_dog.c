b77030bd88e92f87fe57a0520c03dcedeaa15086
Leakim01 <mickael.mangin@live.fr>
2022-10-31 06:33:15 -0700

    Task 1 - Initialize Variables

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stddef.h>
 /**
  * init_dog - Short description
  * @d: pointer
@@ -13,8 +13,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
