368f53917d53188ffe97a7bc9a48794234c6995c
Leakim01 <5662@holbertonstudents.com>
2022-11-23 09:15:28 -0800

    Task 2 - Add a New Node at the Beginning of a List

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Short description
+ * @head: pointer
+ * @n: int à duppliquer
+ *
+ * Description: Longer description
+ *
+ * Return: n
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *p = malloc(sizeof(dlistint_t));
+
+	if (p == NULL)
+		return (NULL);
+	p->n = n;
+	p->next = *head;
+	*head = p;
+
+	return (p);
+}
