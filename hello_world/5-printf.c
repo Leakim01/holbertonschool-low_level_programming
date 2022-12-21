563ff5492901f31bcd18dc6054028c2fa4f9e231
Leakim01 <5662@holbertonstudents.com>
2022-12-21 06:22:36 -0800

    Resolving Error from 6 to 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,20 +5,10 @@
  * printf - Diplay the text
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char c;
-	int i;
-	long int li;
-	long long int lli;
-	float f;
-
-	printf("Size of a char: %zu bytes\n", sizeof(c));
-	printf("Size of an int: %zu bytes\n", sizeof(i));
-	printf("Size of a long int: %zu bytes\n", sizeof(li));
-	printf("Size of a long long int: %zu bytes\n", sizeof(lli));
-	printf("Size of a float: %zu bytes\n", sizeof(f));
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
-
--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,12 +2,22 @@
 
 /**
  * main - Entry point
- * 
+ * printf - Diplay the text
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	putchar(c);
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %zu bytes\n", sizeof(c));
+	printf("Size of an int: %zu bytes\n", sizeof(i));
+	printf("Size of a long int: %zu bytes\n", sizeof(li));
+	printf("Size of a long long int: %zu bytes\n", sizeof(lli));
+	printf("Size of a float: %zu bytes\n", sizeof(f));
 
 	return (0);
 }
