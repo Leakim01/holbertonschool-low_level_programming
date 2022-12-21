50859bf5f523d656eaf3b44e1bd7821173c70fb3
Leakim01 <5662@holbertonstudents.com>
2022-12-21 06:21:04 -0800

    Error from 6 to 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -7,7 +7,17 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %zu bytes\n", sizeof(c));
+	printf("Size of an int: %zu bytes\n", sizeof(i));
+	printf("Size of a long int: %zu bytes\n", sizeof(li));
+	printf("Size of a long long int: %zu bytes\n", sizeof(lli));
+	printf("Size of a float: %zu bytes\n", sizeof(f));
 
 	return (0);
 }
