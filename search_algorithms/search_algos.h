844f802370b09905f63e4e45a7a5eb07682078ff
Leakim01 <5662@holbertonstudents.com>
2023-01-04 05:53:48 -0800

    Task 0 - Function Search Linear

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
