765d273ded24d4392ea6549e0639d3d64dea5d50
Leakim01 <5662@holbertonstudents.com>
2023-01-05 02:08:13 -0800

    Task 1 - FINISH HIM ! FATALITY

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -5,32 +5,48 @@
  * @array: contenu
  * @size : taille
  * @value: valeur
- *
  * Description: Longer description
- *
  * Return: -1
  */
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	size_t i;
 	size_t l = 0;
-	size_t r = size - 1;
-	size_t m = floor((l + r) % 2);
+	size_t r = (size - 1);
 
-	if (array == NULL)
+	if (size <= 0 && array == NULL)
 		return (-1);
-
-	printf("Searching in array: %d", i);
-	while (l <= r)
+	printf("Searching in array: ");
+	for (i = 0; i < r; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[r]);
+	while (l < r)
 	{
-		if (array[m] < value)
-			l = m + 1;
-			
-		else if (array[m] > value)
+		size_t m = ((l + r) / 2);
+
+		if (array[m] > value)
+		{
 			r = m - 1;
+			printf("Searching in array: ");
+			for (i = l; i < r; i++)
+				printf("%d, ", array[i]);
+			printf("%d\n", array[r]);
+			return (r);
+		}
+		else if (array[m] < value)
+		{
+			l = m + 1;
+			printf("Searching in array: ");
+			for (i = l; i < r; i++)
+				printf("%d, ", array[i]);
+			printf("%d\n", array[r]);
+			continue;
+		}
 		else
-				return (m);
+		{
+			return (m);
+		}
 	}
 	return (-1);
 }
